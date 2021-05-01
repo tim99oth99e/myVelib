985843ef6428c7500c6b779b1f860573e2297c27
JeanJaen <jean.bourgeois71@gmail.com>
2021-05-01 16:18:24 +0200

    Add modules Bicycle

---
+++ Core/src/Bicycle.java
@@ -0,0 +1,24 @@
+public class Bicycle {
+    protected Integer Id;
+    protected String typeOfBicycle;
+
+    //Constructors
+    public Bicycle(Integer id, String typeOfBicycle) {
+        Id = id;
+        this.typeOfBicycle = typeOfBicycle;
+    }
+
+    //Getters and Setters
+    public Integer getId() {
+        return Id;
+    }
+    public void setId(Integer id) {
+        Id = id;
+    }
+    public String getTypeOfBicycle() {
+        return typeOfBicycle;
+    }
+    public void setTypeOfBicycle(String typeOfBicycle) {
+        this.typeOfBicycle = typeOfBicycle;
+    }
+}
