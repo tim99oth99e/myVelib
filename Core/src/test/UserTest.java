c47f8b025b6bad368355cb14ad6b3f17c833e3e3
tim99oth99e <timchaumont@gmail.com>
2021-05-06 11:37:00 +0200

    add user test

---
+++ Core/src/test/UserTest.java
@@ -0,0 +1,13 @@
+package src.test;
+
+import src.classes.User;
+import src.enums.RegistrationCardType;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user1 = new User("Jack", 123.0, 145.4, "12353849583740384",
+                RegistrationCardType.None);
+        System.out.println(User.getUsedIds());
+        System.out.println(user1.getId());
+    }
+}
