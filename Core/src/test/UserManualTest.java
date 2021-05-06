47596060fcf7502f74ba4b9604547a23171f6ea6
tim99oth99e <timchaumont@gmail.com>
2021-05-06 17:40:34 +0200

    Modified test

---
+++ Core/src/test/UserManualTest.java
@@ -0,0 +1,18 @@
+package src.test;
+
+import src.classes.*;
+import src.enums.*;
+
+public class UserManualTest {
+    public static void main(String[] args) {
+        User user1 = new User("Jack Wilson", 123.0, 145.4, "12353849583740384",
+                RegistrationCardType.None);
+        System.out.println(User.getUsedIds());
+        System.out.println(user1);
+
+        User user2 = new User("Mark Zuckerberg", 12.0, 15.4, "12353849sfg740384",
+                RegistrationCardType.Vmax);
+        System.out.println(User.getUsedIds());
+        System.out.println(user2);
+    }
+}
