53eca0618628b5e13f9eb190d93068cbe8c57cd8
JeanJaen <jean.bourgeois71@gmail.com>
2021-05-12 21:47:57 +0200

    Updated StationTest

--- Core/src/manualTest/StationManualTest.java
+++ Core/src/manualTest/StationManualTest.java
@@ -21,18 +21,22 @@ public class StationManualTest {
         station1.addParkingSlot(parkingSlotOccupied);
         station1.addParkingSlot(parkingSlotOutOfOrder);
 
+        System.out.println(station1);
+        System.out.println(parkingSlotFree);
+        System.out.println(station1.getParkingSlotHashMap().get(0).getParkingSlotStatus() ==  parkingSlotFree.getParkingSlotStatus());
+
 //        System.out.println(station1.hasBike(TypeOfBicycle.Mechanical));
 //        System.out.println(station1.hasBike(TypeOfBicycle.Electrical));
 
         Station station2 = new Station(6.5, 7.5, StationStatus.OnService, TypeOfStation.Standard);
 //        System.out.println(station2.hasBike());
 
-        System.out.println(station1);
-        System.out.println(station2);
+//        System.out.println(station1);
+//        System.out.println(station2);
 //
 //        Double distance1 = station1.computeDistance(56.5,17.5);
 //        System.out.println(distance1);
-//
+////
 //        Double distance2 = station1.computeDistance(56.5133,17.5);
 //        System.out.println(distance2);
 
--- Core/src/test/StationTest.java
+++ Core/src/test/StationTest.java
@@ -1,5 +1,7 @@
 package src.test;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.*;
 import src.coreClasses.*;
 import src.enums.*;
@@ -7,19 +9,13 @@ import src.enums.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StationTest {
-    Station stationStandard = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Standard);
-    Station stationStandardOffline = new Station(56.5, 17.5, StationStatus.Offline, TypeOfStation.Standard);
-    Station stationPlus = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Plus);
-
-    Bicycle mechanicalBicycle = new Bicycle(TypeOfBicycle.Mechanical);
-
-    ParkingSlot parkingSlotFree = new ParkingSlot(ParkingSlotStatus.Free, null);
-    ParkingSlot parkingSlotOccupied = new ParkingSlot(ParkingSlotStatus.Occupied, mechanicalBicycle);
-    ParkingSlot parkingSlotOutOfOrder = new ParkingSlot(ParkingSlotStatus.OutOfOrder, null);
 
     @Test
-    @DisplayName("Test type of station")
-    public void testTypeOfParkingSlot() {
+    @DisplayName("Test type of stations")
+    public void testTypeOfStations() {
+        Station stationStandard = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Standard);
+        Station stationPlus = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Plus);
+
         assertAll("Type",
                 () -> assertTrue(stationPlus.getTypeOfStation() == TypeOfStation.Plus),
                 () -> assertTrue(stationStandard.getTypeOfStation() == TypeOfStation.Standard)
@@ -29,7 +25,10 @@ class StationTest {
 
     @Test
     @DisplayName("Test status of stations")
-    public void testStatusOfParkingSlot() {
+    public void testStatusOfStations() {
+        Station stationStandard = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Standard);
+        Station stationStandardOffline = new Station(56.5, 17.5, StationStatus.Offline, TypeOfStation.Standard);
+
         assertAll("Type",
                 () -> assertTrue(stationStandardOffline.getStationStatus() == StationStatus.Offline),
                 () -> assertTrue(stationStandard.getStationStatus() == StationStatus.OnService)
@@ -39,22 +38,35 @@ class StationTest {
     @Test
     @DisplayName("Test unique ID for stations")
     public void testUniqueID() {
+        Station stationStandard = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Standard);
+        Station stationStandardOffline = new Station(56.5, 17.5, StationStatus.Offline, TypeOfStation.Standard);
+        Station stationPlus = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Plus);
+
         assertAll("unique ID",
                 () -> assertTrue(stationStandard.getId() != stationStandardOffline.getId()),
                 () -> assertTrue(stationStandard.getId() != stationPlus.getId())
         );
     }
 
+
+
     @Test
-    void addParkingSlot() {
+    @DisplayName("Test of the method computeDistance")
+    public void testComputeDistance() {
+        Station stationStandard = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Standard);
+        Station stationPlus = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Plus);
+
+        assertAll("Distance must be calculated with a maximum error of 10 meters",
+                () -> assertTrue(stationStandard.computeDistance(56.5, 17.5) >= 0
+                        && stationStandard.computeDistance(56.5, 17.5) <= 10),
+                () -> assertTrue(stationStandard.computeDistance(56.5133, 17.5) >= 1469
+                        && stationStandard.computeDistance(56.5133, 17.5) <= 1489)
+        );
     }
 
     @Test
-    void computeDistance() {
-    }
-
-    @Test
-    void hasBike() {
+    @DisplayName("Test of the method hasBike")
+    public void testHasBike() {
     }
 
     @Test
@@ -66,6 +78,59 @@ class StationTest {
     }
 
     @Test
-    void getNumberOfBike() {
+    @DisplayName("Test of the method addParkingSlot")
+    public void testAddParkingSlot() {
+        Station stationStandard = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Standard);
+
+        Bicycle mechanicalBicycle = new Bicycle(TypeOfBicycle.Mechanical);
+
+        ParkingSlot parkingSlotFree = new ParkingSlot(ParkingSlotStatus.Free, null);
+        ParkingSlot parkingSlotOccupied = new ParkingSlot(ParkingSlotStatus.Occupied, mechanicalBicycle);
+        ParkingSlot parkingSlotOutOfOrder = new ParkingSlot(ParkingSlotStatus.OutOfOrder, null);
+
+        stationStandard.addParkingSlot(parkingSlotFree);
+        stationStandard.addParkingSlot(parkingSlotOccupied);
+        stationStandard.addParkingSlot(parkingSlotOutOfOrder);
+
+        assertEquals(stationStandard.getParkingSlotHashMap().size(),3);
+    }
+
+
+    @Test
+    @DisplayName("Test of the method getNumberOfBike")
+    public void testGetNumberOfBike() {
+        Station stationFree = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Standard);
+        Station stationFull = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Standard);
+        Station stationMix = new Station(56.5, 17.5, StationStatus.OnService, TypeOfStation.Plus);
+
+        Bicycle electricalBicycle1 = new Bicycle(TypeOfBicycle.Electrical);
+        Bicycle electricalBicycle2 = new Bicycle(TypeOfBicycle.Electrical);
+        Bicycle mechanicalBicycle = new Bicycle(TypeOfBicycle.Mechanical);
+
+
+        ParkingSlot parkingSlotFree1 = new ParkingSlot(ParkingSlotStatus.Free, null);
+        ParkingSlot parkingSlotFree2 = new ParkingSlot(ParkingSlotStatus.Free, null);
+        ParkingSlot parkingSlotOccupiedMechanical = new ParkingSlot(ParkingSlotStatus.Occupied, mechanicalBicycle);
+        ParkingSlot parkingSlotOccupiedElectrical1 = new ParkingSlot(ParkingSlotStatus.Occupied, electricalBicycle1);
+        ParkingSlot parkingSlotOccupiedElectrical2 = new ParkingSlot(ParkingSlotStatus.Occupied, electricalBicycle2);
+
+        stationFree.addParkingSlot(parkingSlotFree1);
+        stationFree.addParkingSlot(parkingSlotFree2);
+
+        stationFull.addParkingSlot(parkingSlotOccupiedElectrical1);
+        stationFull.addParkingSlot(parkingSlotOccupiedElectrical2);
+        stationFull.addParkingSlot(parkingSlotOccupiedMechanical);
+
+        stationMix.addParkingSlot(parkingSlotFree1);
+        stationMix.addParkingSlot(parkingSlotOccupiedElectrical1);
+        stationMix.addParkingSlot(parkingSlotOccupiedMechanical);
+        stationMix.addParkingSlot(parkingSlotFree2);
+
+        assertAll("Correct number of bicycle within the station",
+                () -> assertTrue(stationFree.getNumberOfBike(TypeOfBicycle.Mechanical) == 0),
+                () -> assertTrue(stationFull.getNumberOfBike(TypeOfBicycle.Mechanical) == 1),
+                () -> assertTrue(stationFull.getNumberOfBike(TypeOfBicycle.Electrical) == 2),
+                () -> assertTrue(stationMix.getNumberOfBike(TypeOfBicycle.Mechanical) == 1)
+        );
     }
 }
