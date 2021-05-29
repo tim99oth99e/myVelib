56ae0961408d2aca592add00eea1d29222a7f965
tim99oth99e <timchaumont@gmail.com>
2021-05-29 15:08:25 +0200

    add VelibCommand object

---
+++ UI/src/VelibCommand.java
@@ -0,0 +1,24 @@
+package src;
+
+import java.util.ArrayList;
+
+public class VelibCommand {
+    private String commandName;
+    private ArrayList<String> arguments;
+
+    public VelibCommand(String commandName, ArrayList<String> arguments) {
+        this.commandName = commandName;
+        this.arguments = arguments;
+    }
+
+    public void eval() {
+//        switch (commandName) {
+//            case "addUser":
+                // ajouter
+//                Record.addUser(....)
+//                break;
+//            case "":
+
+        }
+    }
+}
