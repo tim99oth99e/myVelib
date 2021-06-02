2b51b1c6aa5849751f6b09943a4f4128a8fa5ec6
tim99oth99e <timchaumont@gmail.com>
2021-06-02 14:53:20 +0200

    add javadoc to Registration Card classes

--- Core/src/registrationCard/VlibreRegistrationCard.java
+++ Core/src/registrationCard/VlibreRegistrationCard.java
@@ -3,8 +3,14 @@ package src.registrationCard;
 import src.coreClasses.User;
 import src.enums.TypeOfBicycle;
 
+/**
+ * This class describes a Vlibre registration card.
+ */
 public class VlibreRegistrationCard extends RegistrationCard {
 
+    /**
+     * Instantiates a new Vlibre registration card.
+     */
     public VlibreRegistrationCard() {
         super(true, 0, 1,1,2);
     }
